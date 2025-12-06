e929083188348eb1bb41d34d8829f0e8054e4695
JopraZ <louisdemol9@gmail.com>
2025-12-06 22:19:23 +0100

    card v1.2

--- src/component/modale.jsx
+++ src/component/modale.jsx
@@ -1,13 +1,27 @@
-
+import Card from "react-bootstrap/Card";
+import "../component/modale.css"
 export default function Modale({ isOpen, onClose }) {
     if (!isOpen) return null;
     
     return(
         <>
         <div className="modaleStyle">
-            <button onClick={onClose}className="btn.modal">X</button>
-            <p>Modale</p>
+         <Card style = {{width: '150ox'}}>
+            <Card.Body>
+                <div >
+                    <button onClick={onClose}className="btn.modal">X</button>
+                    <Card.Title>Mon profil GitHub</Card.Title>
+                </div>
+                <Card.Img variant="left" src="../../public/assets/portfolio/avatar.png" />
+                <Card.Link href='https://github.com/JopraZ' target="blank">John Doe</Card.Link>
+                <Card.Text>As we all know John Doe's idendity is unknow. Ijust wanted to contribute without being known.</Card.Text>
+                <Card.Text>Repositories : 1</Card.Text>
+                <Card.Text>Followers: 36</Card.Text>
+                <Card.Text>Following: 0</Card.Text>
+                <button onClick={onClose}className="btn.modal">Fermer</button>
+            </Card.Body>           
+        </Card>
         </div>
         </>
     )
-}
+};
